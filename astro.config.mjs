ff0e1bc61f3a4cb32d6c97c62fd9a9187b9a6735
Prima <aprita.primayuda@gmail.com>
2023-11-12 17:49:23 +0700

    Revise Hunghung & Agung profile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   output: "server",
   adapter: vercel({
-    edgeMiddleware: true,
+    edgeMiddleware: true
   })
 });
