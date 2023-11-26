bd8158280a98c203ecbe2b3e8053114f5a800271
Prima <aprita.primayuda@gmail.com>
2023-11-26 17:58:57 +0700

    Adding Anggota & News

---
+++ src/components/sections/datas.ts
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        id: 1,
+        image: "/images/activities/APDI-6.jpg",
+        date: "20 Desember 2021",
+        title: "APDI resmi terbentuk",
+        content: "Berawal dari inisiasi para Alumni Digital Leadership Academy (DLA) 2021, kini Asosiasi Pemimpin Digital Indonesia resmi terbentuk. Pembentukan APDI sendiri berlangsung secara hybrid dalam acara gala dinner di Jakarta.",
+    },
+    {
+        id: 2,
+        image: "/images/activities/APDI-4.jpg",
+        date: "22 Desember 2021",
+        title: "Diversifikasi Pangan Indonesia",
+        content: "Diversifikasi Pangan, Salah Satu Upaya Indonesia Berdikari Pangan, Kelompok 5 alumni Digital Leadership Academy (DLA) x Tsinghua University berkunjung ke daerah Pelaihari, Kabupaten Tanah Laut. Bupati Kabupaten Tanah Laut, Sukamta beserta jajarannya menyambut hangat kehadiran Tim Visitasi di Pendopo Kabupaten Tanah Laut dan menerima dengan tangan terbuka bagi siapapun yang ingin membuat Kabupaten Tanah Laut menjadi daerah yang maju.",
+    },
+    {
+        id: 3,
+        image: "/images/activities/APDI-7.jpg",
+        date: "6 Desember 2021",
+        title: "Kunjungan ke Tanah Laut",
+        content: "Seusai kunjungan ke ke Kota Banjarmasin, Kelompok 5 alumni Digital Leadership Academy (DLA) x Tsinghua University berkunjung ke Kabupaten Tanah Laut dan diterima oleh Bupati Tanah Laut",
+    },
+    {
+        id: 4,
+        image: "/images/activities/APDI-5.jpg",
+        date: "15 Desember 2021",
+        title: "Strategi Ekosistem Digital",
+        content: "Indonesia mempunyai potensi pasar digital yang sangat besar dan terus berkembang cepat. Oleh karena itu, Presiden Jokowi menekankan pentingnya menyiapkan strategi untuk mengambil kesempatan di tengah kemajuan dunia digital.",
+    },
+]
